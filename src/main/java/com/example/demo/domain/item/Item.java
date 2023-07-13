af7a7a510c8e48c2a99cc850e1dfc330a4830f19
kimsolee95 <ahhasolee@naver.com>
2023-07-13 12:33:27 +0900

    feat: Setter 대신 엔티티 내의 비즈니스 메서드를 활용하여 값 변경되게끔 코드 추가

--- src/main/java/com/example/demo/domain/item/Item.java
+++ src/main/java/com/example/demo/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.item;
 
 import com.example.demo.domain.Category;
+import com.example.demo.exception.NotEnoughStockException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
@@ -12,12 +13,11 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
   @Id
@@ -31,4 +31,22 @@ public abstract class Item {
 
   @ManyToMany(mappedBy = "items")
   private List<Category> categories = new ArrayList<>();
+
+  /**
+   * stock 증가
+   * */
+  public void addStock(int quantity) {
+    this.stockQuantity += quantity;
+  }
+
+  /**
+   * stock 감소
+   * */
+  public void removeStock(int quantity) {
+    int restStock = this.stockQuantity - quantity;
+    if (restStock < 0) {
+      throw new NotEnoughStockException("need more stock");
+    }
+    this.stockQuantity = restStock;
+  }
 }
--- src/test/java/com/example/demo/service/MemberServiceTest.java
+++ src/test/java/com/example/demo/service/MemberServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.demo.domain.Member;
 import com.example.demo.repository.MemberRepository;
+import javax.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
   @Autowired MemberService memberService;
   @Autowired MemberRepository memberRepository;
+  @Autowired EntityManager em;
 
   @Test
   public void member_join() throws Exception {
@@ -27,16 +29,22 @@ import org.springframework.transaction.annotation.Transactional;
     //when
     Long saveId = memberService.join(member);
     //then
+    em.flush();
     assertEquals(member, memberRepository.findOne(saveId));
   }
 
   @Test
   public void duplicate_member_exception() throws Exception {
     //given
-
+    Member member1 = new Member();
+    member1.setName("kim1");
+    Member member2 = new Member();
+    member2.setName("kim2");
     //when
-
+    memberService.join(member1);
+    memberService.join(member2);
     //then
+    fail("예외 발생!");
   }
 
 }
