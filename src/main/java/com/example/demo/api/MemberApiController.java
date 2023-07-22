0f7435f50f1c744efee4191f7a54e1bb2f1c6faf
kimsolee95 <ahhasolee@naver.com>
2023-07-23 08:26:15 +0900

    feat: 회원 목록 조회 컨트롤러 추가

--- src/main/java/com/example/demo/api/MemberApiController.java
+++ src/main/java/com/example/demo/api/MemberApiController.java
@@ -2,10 +2,16 @@ package com.example.demo.api;
 
 import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +21,68 @@ public class MemberApiController {
 
   private final MemberService memberService;
 
+  @GetMapping("/api/v2/members")
+  public Result memberV2() {
+    List<Member> findMembers = memberService.findMembers();
+    List<MemberDto> collect = findMembers.stream()
+        .map(m -> new MemberDto((m.getName())))
+        .collect(Collectors.toList());
+    return new Result(collect);
+  }
+
+  /**
+   * worst ex -> 엔티티를 param vo로 활용
+   * -> 엔티티의 스펙은 변경되면 안 되는데 parameter로 활용하면 위험 부담이 있다.
+   * */
   @PostMapping("/api/v1/members")
   public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
     return new CreateMemberResponse(memberService.join(member));
+  }
+
+  @PostMapping("/api/v2/members")
+  public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+
+    Member member = new Member();
+    member.setName(request.getName());
+    return new CreateMemberResponse(memberService.join(member));
+  }
+
+  @PutMapping("/api/v2/members/{id}")
+  public UpdateMemberResponse updateMemberV2(
+      @PathVariable("id") Long id,
+      @RequestBody @Valid UpdateMemberRequest request) {
+    memberService.update(id, request.getName());
+    Member findMember = memberService.findOne(id);
+    return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class MemberDto {
+    private String name;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class Result<T> {
+    private T data;
+  }
+
+  @Data
+  static class UpdateMemberRequest {
+    private String name;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class UpdateMemberResponse {
+    private Long id;
+    private String name;
+  }
+
+  @Data
+  static class CreateMemberRequest {
+    private String name;
   }
 
   @Data
