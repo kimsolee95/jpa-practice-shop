676654a0640f68edbd1e8a512e02d2ebbfe5f83d
kimsolee95 <ahhasolee@naver.com>
2023-07-27 23:05:25 +0900

    feat: 페이징이 가능하도록 yaml 설정 추가

--- src/main/java/com/example/demo/api/OrderApiController.java
+++ src/main/java/com/example/demo/api/OrderApiController.java
@@ -12,6 +12,7 @@ import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -54,6 +55,17 @@ public class OrderApiController {
       System.out.println("order ref= " + order + " id= " + order.getId());
     }
 
+    List<OrderDto> result = orders.stream()
+        .map(o -> new OrderDto(o))
+        .collect(Collectors.toList());
+    return result;
+  }
+
+  @GetMapping("/api/v3.1/orders")
+  public List<OrderDto> orderV3_page(
+      @RequestParam(value = "offset", defaultValue = "0") int offset,
+      @RequestParam(value = "limit", defaultValue = "100")int limit) {
+    List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
     List<OrderDto> result = orders.stream()
         .map(o -> new OrderDto(o))
         .collect(Collectors.toList());
